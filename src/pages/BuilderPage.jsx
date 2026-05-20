60a4f0ba447c3c61042e6c6e0f62ef487b8f8dfa
Newjul <juleswiseman7@gmail.com>
2026-05-20 17:05:55 +0200

    added alternating feature between editor and preview but the preview not yet working

--- src/pages/BuilderPage.jsx
+++ src/pages/BuilderPage.jsx
@@ -5,6 +5,8 @@ import PropertiesPanel from "../components/PropertiesPanel";
 import { useState } from "react";
 
 export default function BuilderPage(){
+  const [mode, setMode] = useState("edit");
+  const [currentNodeId, setCurrentNodeId] = useState("1");
 
   const {meta, nodes} = flow;
   const {w, h} = meta.canvas_size;
@@ -12,11 +14,23 @@ export default function BuilderPage(){
   const [selectNodeId, setSelectNodeId]=useState(null);
   const [nodeState, setNodestate]=useState(nodes);
 
+  const nodeMap = Object.fromEntries(
+    nodeState.map((n)=>[n.id, n])
+  );
+
   const selectedNode = nodeState.find((n) => 
     n.id === selectNodeId);
 
+  const currentNode = nodeMap[currentNodeId];
+
   return(
     <div className="bg-gray-200 flex-1 overflow-auto p-6">
+        <button
+           onClick={() => setMode(mode === "edit" ? "preview":"edit")}
+           className="absolute top-4 left-4 z-50 bg-black text-white px-3 py-1 rounded">
+            {mode === "edit" ? "Play Preview":"Back to Editor"}
+           </button>
+     { mode === "edit" ? (<>
       <div className="relative bg-white shadow-xl"
        style={{
         width:w,
@@ -39,6 +53,11 @@ export default function BuilderPage(){
          selectedNode={selectedNode}
          nodesState={nodeState}
          setNodesState={setNodestate}/>
+      </>):(<div>
+        <h2 className="text-color-black">Previw pannel</h2>
+      </div>)}
+        
     </div>
-  )
+    
+)
 }
